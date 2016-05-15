f2d5070e7db8e953b7c8822f90e3fc8930d2d28d
ang6190 <allison.ng@stonybrook.edu>
2016-05-15 14:12:24 -0400

    trying to pull
    made a few fixes to parallel park, but messed up the actual parallel
    parking algo

--- src/ParallelPark.java
+++ src/ParallelPark.java
@@ -7,7 +7,7 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class ParallelPark {
 	public static final int WHEEL_DIAMETER = 56;
 	public static final int ROBOT_LENGTH = 180;
-	public static final int DISTANCE_TO_SIDEWALK = 20;
+	public static final int DISTANCE_TO_SIDEWALK = 25;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UltrasonicSensor sens = new UltrasonicSensor(SensorPort.S1);
@@ -19,15 +19,16 @@ public class ParallelPark {
 		System.out.println(sens.getDistance());
 		double distance = Integer.MIN_VALUE;
 		
-		int error = 10;
+		int error = 5;
 		boolean found_spot = false;
 		while(!canFit(distance)){
 			Motor.A.resetTachoCount();
 			Motor.C.resetTachoCount();
 			pilot.forward();
 			//closest thing is sidewalk
-			if(sens.getDistance() >= DISTANCE_TO_SIDEWALK){
-				while (sens.getDistance() >= DISTANCE_TO_SIDEWALK) 
+			if(sens.getDistance() >= DISTANCE_TO_SIDEWALK - error){
+				// continue until an obstacle is reached
+				while (sens.getDistance() >= DISTANCE_TO_SIDEWALK - error) 
 					System.out.println(sens.getDistance());
 				
 				//if(sens.getDistance() > DISTANCE_TO_SIDEWALK)
@@ -46,24 +47,27 @@ public class ParallelPark {
 		System.out.println("length : "+distance);
 		System.out.println(canFit(distance));
 		if (canFit(distance))
-			startParking(pilot);
+			startParallelParking(pilot, sens);
 		Button.waitForAnyPress();
 	}
 	
-	public static void startParking(DifferentialPilot pilot) {
+	public static void startParallelParking(DifferentialPilot pilot, UltrasonicSensor sensor) {
 		//pilot.travel(-120);
 		//pilot.rotate(45);
 		pilot.travel(-90);
 		pilot.travelArc(-120, -100);
 		pilot.travel(-130);
+		System.out.println(sensor.getDistance());
+		//pilot.travelArc(150, -75);
+		//pilot.travel(-50);
 		pilot.travelArc(100, -75);
-		//pilot.travelArc(-100, 25);
 		pilot.travel(50);
+		System.out.println(sensor.getDistance());
 		//pilot.rotate(-45);
 		//pilot.travel(30);
 	}
 	public static double convertTachosToMilli(int tachos){
-		return (tachos/360)*Math.PI*WHEEL_DIAMETER;
+		return (tachos/360.0)*Math.PI*WHEEL_DIAMETER;
 	}
 	
 	public static boolean canFit(double length) {
